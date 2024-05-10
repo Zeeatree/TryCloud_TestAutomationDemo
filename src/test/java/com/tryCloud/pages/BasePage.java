f9740e1175f3fb442cd80405901ef371cd34d7ab
Zeeatree <zulpikar27@gmail.com>
2024-05-10 15:51:59 -0400

    Create a new feature file for my DashboardPage.java made changes to my login feature and login step definitions to follow the naming convention

---
+++ src/test/java/com/tryCloud/pages/BasePage.java
@@ -0,0 +1,27 @@
+package com.tryCloud.pages;
+
+import com.tryCloud.utilities.BrowserUtils;
+import com.tryCloud.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public abstract class BasePage {
+
+    private WebDriverWait wait;
+
+    public BasePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public void navigateTo(String module){
+        module=module.toLowerCase();
+        String locator="//ul[@id='appmenu']/li[@data-id='"+module+"']";
+        WebElement moduleTab=Driver.getDriver().findElement(By.xpath(locator));
+        BrowserUtils.waitForClickablility(moduleTab,10);
+        moduleTab.click();
+    }
+}
---
+++ src/test/java/com/tryCloud/pages/DashboardPage.java
@@ -0,0 +1,55 @@
+package com.tryCloud.pages;
+
+import com.tryCloud.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class DashboardPage {
+
+    public DashboardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li[position() <= 7]/a")
+    public List<WebElement> modules;
+
+    @FindBy(xpath = "//*[@id='searchbox']")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//*[@id = 'fileList']/tr")
+    public List<WebElement> searchResults;
+
+    @FindBy(xpath = "//div[@aria-controls='notification-container']")
+    public WebElement notificationsIcon;
+
+    @FindBy(xpath = "//div[@class='icon icon-notifications-dark']")
+    public WebElement Notifications;
+
+    @FindBy(xpath = "//*[@id='expand']")
+    public WebElement userIcon;
+
+    @FindBy(xpath = "//*[@id=\"expanddiv\"]/ul/li")
+    public List<WebElement> userOptions;
+
+    @FindBy(xpath = "//div[@class='icon-contacts menutoggle']")
+    public WebElement contactsIcon;
+
+    @FindBy(xpath = "//*[@id='contactsmenu-menu']")
+    public WebElement ContactsMenu;
+
+
+
+    
+
+
+
+
+
+
+    
+    
+    
+}
